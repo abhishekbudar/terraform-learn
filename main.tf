813bf198197508bddd06034e61e56a0e6136b2c3
abhi <abhishekbudar28@gmail.com>
2023-02-07 20:58:39 +0530

    ec2 deployment

--- main.tf
+++ main.tf
@@ -22,6 +22,8 @@ variable "my_ip"{
 variable "instance_type"{
 }
 
+variable "public_key_location"{}
+
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
     tags = {
@@ -101,7 +103,13 @@ resource "aws_security_group" "myapp-sg" {
   }
 }
 
-resource "aws_instance" "myapp-server" {
+resource "aws_key_pair" "ssh-key"{
+
+    key_name = "server-key"
+    public_key = "${var.public_key_location}"
+}
+
+resource "aws_instance" "myapp-server1" {
     ami = data.aws_ami.latest-amazon-linux-image.id
     instance_type = var.instance_type
     subnet_id = aws_subnet.myapp-subnet-1.id
@@ -109,7 +117,17 @@ resource "aws_instance" "myapp-server" {
     availability_zone = "ap-south-1a"
 
     associate_public_ip_address = true
-    key_name = "server-key-pairs" 
+    key_name = aws_key_pair.ssh-key.key_name
+
+    user_data = <<EOF
+                    #!/bin/bash
+                    sudo yum update -y
+                    sudo yum install -y docker
+                    sudo systemctl start docker
+                    sudo usermod -aG docker ec2-user
+                    docker run -p 8080:80 nginx
+                EOF
+
     tags = {
     Name = "${var.env_prefix}-server"
   }
@@ -128,13 +146,14 @@ data "aws_ami" "latest-amazon-linux-image"{
         name ="virtualization-type"
         values = ["hvm"]
     }
-
 }
 
-# output "ami-id"{
 
-#     value = data.aws_ami.latest-amazon-linux-image
-# }
+
+output "public-ip"{
+
+    value = aws_instance.myapp-server1.public_ip
+ }
 
 # data "aws_vpc" "existing_vpc" {
 #     default = true
