078b0be9fc46b04d708ed70e3591f36a50eff7ef
abhi <abhishekbudar28@gmail.com>
2023-02-10 11:59:52 +0530

    remote s3 tf state

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+
+    required_version = ">= 0.12"
+    backend "s3" {
+        bucket = "myapp-bucket"
+        key = "myapp/state.tfstate"
+    }
+}
+
 provider "aws" {
     region ="ap-south-1"
 }
